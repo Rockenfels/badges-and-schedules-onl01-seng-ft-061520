484ccf2d0fa4c3e98cc1f543003e297b9c5e9f3c
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-17 15:18:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages << badge_maker(name)
+  end
+  messages
+end
+
+def assign_rooms(names)
+  messages = []
+  names.each_with_index do |name, index| 
+    messages << "Hello, #{name}! You'll be assigned to room #{(index + 1)}!"
+  end
+  messages
+end
+
+def printer(names)
+  puts batch_badge_creator(names)
+  puts assign_rooms(names)
+end
