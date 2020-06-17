8586d3ab6578dbcd3cc65daa17cc7445f0ba5b45
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-17 15:19:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  puts batch_badge_creator(names)
-  puts assign_rooms(names)
+  badges = batch_badge_creator(names)
+  rooms = assign_rooms(names)
 end
